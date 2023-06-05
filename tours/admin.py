5bc9d010ae9af47d418fe026ac6c10fc065fdf39
lpewton <>
2023-06-05 10:52:28 +0000

    Add tour model and admin

--- tours/admin.py
+++ tours/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from .models import Continent
+from .models import Continent, Tour
 
 class ContinentAdmin(admin.ModelAdmin):
     list_display = (
         'continent',
     )
 
+class TourAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'location', 'start_date', 'end_date', 'continent',
+    )
+
 admin.site.register(Continent, ContinentAdmin)
+admin.site.register(Tour, TourAdmin)
 
--- tours/models.py
+++ tours/models.py
@@ -1,8 +1,28 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Continent(models.Model):
-    continent = models.CharField(max_length=254)
+    continent = models.CharField(max_length=50)
     image = models.ImageField(null=True, blank=True)
     
     def __str__(self):
         return self.continent
+
+class Tour(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=50)
+    continent = models.ForeignKey(Continent, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    slots = models.PositiveIntegerField(
+        validators=[MinValueValidator(1)], default=1)
+    slots_left = models.PositiveIntegerField(default=1)
+    description = models.CharField(max_length=254)
+    rating = models.FloatField(
+        validators=[MinValueValidator(0)], default=0)
+    image_1 = models.ImageField(null=True, blank=True)
+    image_2 = models.ImageField(null=True, blank=True)
+    image_3 = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
