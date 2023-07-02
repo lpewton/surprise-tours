89a9e0bd84027ef2958e7064eec6e6cafda7d0c2
lpewton <>
2023-07-02 13:04:30 +0000

    Allow user to purchase even if they're not logged in

--- checkout/models.py
+++ checkout/models.py
@@ -22,7 +22,7 @@ class Order(models.Model):
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
-    show_date = models.DateField()
+    show_date = models.DateField(auto_now_add=True)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     original_bag = models.TextField(null=False, blank=False, default='')
     stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
