fefa2b38582a911315be3f829fbda1068488ee3d
lpewton <>
2023-07-06 00:29:42 +0000

    Create contact model

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class contactModel(models.Model):
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    subject = models.CharField(max_length=100, null=False, blank=False)
+    message = models.CharField(max_length=300, null=False, blank=False)
+
+    def __str__(self):
+        return self.full_name
