5931798ba5ccd8abae0b5268f047fdc107e8f3ef
lpewton <>
2023-07-09 22:58:55 +0000

    Fix responsiveness for all forms

--- checkout/forms.py
+++ checkout/forms.py
@@ -35,5 +35,5 @@ class OrderForm(forms.ModelForm):
                 else:
                     placeholder = placeholders[field]
             self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'form-control my-3'
+            self.fields[field].widget.attrs['class'] = 'checkout-form my-3'
             self.fields[field].label = False
